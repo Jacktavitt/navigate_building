e703500be35b29ac62a24f32f82600c7d7d19d73
John Feilmeier <jacktavitt@gmail.com>
2018-07-27 09:56:33 -0700

    added ADA specs and hallway maker

---
+++ source/ImageGeneration/hall_driver.py
@@ -0,0 +1,38 @@
+"""
+Driver for generating hallway for better training data.
+"""
+import DataGenerator as DAG
+import CustomImage as CIM
+import cv2
+import numpy as np
+import os
+import random
+from argparse import ArgumentParser
+
+
+
+
+def main(directory):
+    IMAGE_CLASS = CIM.GeneratedImage
+    IMG_GENR = DAG.ImageGenerator(IMAGE_CLASS,plaqueSize=75, resolution=10, randSeed=42)
+
+    hw=IMG_GENR.make_hallway()
+    hw.save(imagePath=''.join([directory,'test_hallway.png']))
+
+
+if __name__=='__main__':
+    parser=ArgumentParser()
+    parser.add_argument('--directory','-d', 
+                        help = 'directory where images will be saved',
+                        required=True)
+    # parser.add_argument('--images', '-i', 
+    #                     help = 'how many images to make',
+    #                     required = True)
+    # parser.add_argument('--noise', '-n', 
+    #                     help = 'how noisy should images be',
+    #                     default=2)
+    # parser.add_argument('--seed', '-s', 
+    #                     default=random.randint(0,10),
+    #                     help='Seed for random numbers')
+    args=parser.parse_args()
+    main(args.directory)
---
+++ source/Modules/ADA.py
@@ -0,0 +1,35 @@
+'''collection of ADA requirements.
+ALL IN INCHES
+'''
+import CustomErrors as CER
+
+def get_font_size(text, signWidth):
+    '''returns sign spec from ADA.
+    in INCHES!
+    usage= sizeChart[signWidth][charnum]
+    '''
+    charnum = len(text)
+    if charnum > 26:
+        raise CER.PlaqueFontError(1)
+    elif signWidth > 19:
+        raise CER.PlaqueFontError(2)
+
+    sizeChart= {
+        4:{5: 0.625, 4: 0.75, 3: 1},
+        6:{7: 0.625, 6: 0.75, 5: 0.875, 4: 1.25, 3: 1.5},
+        8:{11: 0.625, 9: 0.75, 8: 0.875, 7: 1, 5: 1.25, 4: 1.5},
+        10:{14: 0.625, 11: 0.75, 10: 0.875, 9: 1, 8: 1.25, 7: 1.5},
+        12:{18: 0.625, 14: 0.75, 12: 0.875, 11: 1, 8: 1.25, 7: 1.5},
+        18:{25: 0.625, 21: 0.75, 18: 0.875, 16: 1, 13: 1.25, 11: 1.5}
+    }
+    keys = list(sizeChart.keys())
+    chartKey = min(keys, key=lambda x: abs(x-signWidth))
+    chartKeyKeys = list(sizeChart[chartKey])
+    fontKey = min(chartKeyKeys, key = lambda x: abs(x-charnum))
+    return sizeChart[chartKey][fontKey]
+
+DOOR_HT = 80
+DOOR_WD = 32
+VEIL_HT = 120
+PQ_HT = 50
+CEIL_HT = 120
