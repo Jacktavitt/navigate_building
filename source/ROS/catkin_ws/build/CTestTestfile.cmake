0806891e7a81f46f04d95c3031652461248b3229
jacktavitt <jacktavitt@gmail.com>
2018-10-21 16:24:37 -0700

    attempt to compile cv_bridge for python3 (unsuccessful)

--- source/ROS/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/johnny/Documents/navigate_building/source/ROS/catkin_ws/src
-# Build directory: /home/johnny/Documents/navigate_building/source/ROS/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("image_subscriber")
--- source/ROS/catkin_ws/build/image_subscriber/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/johnny/Documents/navigate_building/source/ROS/catkin_ws/src/image_subscriber
-# Build directory: /home/johnny/Documents/navigate_building/source/ROS/catkin_ws/build/image_subscriber
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
